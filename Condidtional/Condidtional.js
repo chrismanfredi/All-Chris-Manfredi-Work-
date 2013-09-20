f2b7adb68195ed9f991715515846239195d613b0
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-09-20 19:45:02 -0400

    added folders
    added folders

---
+++ Condidtional/Condidtional.js
@@ -0,0 +1,57 @@
+// Last Chance for gas!
+
+
+//var gas = 150;
+//var station = 200;
+
+//if you have less then 200 miles in your tank, Print to the console "YOU NEED GAS NOW!"
+//if the tank has less the 200 miles
+//if (gas < station){
+//    //code performed if the condition is true
+//    console.log("YOU NEED GAS NOW!")
+
+//}
+
+
+
+
+
+// Grade Letter Calculator
+
+//var g = 80;
+
+//if (g = 80)
+//{
+    //code performed if the condition is true
+//    console.log("You have a 80%, which means you have earned a B- in the class!");
+
+//}
+
+
+// Movie Ticket Price
+
+// var movie_time = 10;
+// var age = 18;
+
+
+//
+
+
+//if ( movie_time > 5)
+//{
+    //code performed if the condition is true
+//    console.log("Ticket price is 12 Dollars")
+
+//}
+
+//if (age < 10)
+//{
+//    //code performed if the condition is true
+//    console.log("The ticket price is 7 dollars")
+//}
+
+
+
+
+
+
