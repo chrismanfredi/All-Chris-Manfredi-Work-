1eaa9bf460d183203928993a6d6e4b4c5e311a13
Chris Manfredi <cmbaseball94@fullsail.edu>
2013-09-22 22:40:40 -0400

    Functions
    Functions

---
+++ Functions/Functions.js
@@ -0,0 +1,17 @@
+//Calculate the curcumference of a circle
+
+
+// The formula to calculate the circumference of a circle is C = D * pi
+
+function circumference ()
+{
+
+    var Diameter = 16;
+    var pi = 3.14;
+    var answer = Diameter * pi;
+
+}
+
+
+
+
